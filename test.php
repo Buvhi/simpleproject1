001e3c331599e2222d396fb6375a3acf06625be1
buvanesh05 <buvaneshwaran00510@gmail.com>
2022-10-27 21:36:16 +0530

    sample push

--- test.php
+++ test.php
@@ -1,171 +1,60 @@
-<html lang="en"><head></head><body data-new-gr-c-s-check-loaded="14.1084.0" data-gr-ext-installed=""><pre></pre>
 
-    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-        
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.104.2">
-    <title>Carousel Template · Bootstrap v5.2</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/carousel/">
-
-    
-
-    
-
-
-
-    <!-- Favicons -->
-<link rel="apple-touch-icon" href="/docs/5.2/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-<link rel="icon" href="/docs/5.2/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-<link rel="icon" href="/docs/5.2/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-<link rel="manifest" href="/docs/5.2/assets/img/favicons/manifest.json">
-<link rel="mask-icon" href="/docs/5.2/assets/img/favicons/safari-pinned-tab.svg" color="#712cf9">
-<link rel="icon" href="/docs/5.2/assets/img/favicons/favicon.ico">
-<meta name="theme-color" content="#712cf9">
-
-
-    <style>
-      
-
-
-.form-signup {
-    width: 100%;
-    max-width: 330px;
-    padding: 15px;
-    margin: auto;
-}
-
-.form-signup .checkbox {
-    font-weight: 400;
-}
-
-.form-signup .form-floating:focus-within {
-    z-index: 2;
-}
-
-.form-signup input[type="email"] {
-    margin-bottom: -1px;
-    border-bottom-right-radius: 0;
-    border-bottom-left-radius: 0;
-    border-top-left-radius: 0;
-    border-top-right-radius: 0;
-}
-
-.form-signup input[type="password"] {
-    margin-bottom: 10px;
-    border-top-left-radius: 0;
-    border-top-right-radius: 0;
-}
-
-.form-signup input[name="phone"] {
-    margin-bottom: -1px;
-    border-bottom-right-radius: 0;
-    border-bottom-left-radius: 0;
-    border-top-left-radius: 0;
-    border-top-right-radius: 0;
-}
-
-.form-signup input[name="username"] {
-    margin-bottom: -1px;
-    border-bottom-right-radius: 0;
-    border-bottom-left-radius: 0;
-}
-    </style>
-
-    
-    <!-- Custom styles for this template -->
-    <link href="carousel.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-          
-        <div class="container">
   
-<header>
-<header>
-  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">SNA Hotel</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav me-auto mb-2 mb-md-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Convention Halls </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled">Restaurants</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled">Careers</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled">contact Us</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled">About Us</a>
-          </li>
-        </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+    <div class="hero-background">
+      <div class="row">
+        <div class="columns small-12 medium-8">
+          <h1>CONTACT US </h1>
+          <p>The overlay is the building block - the rest of this is just context to show how you might use it</p>
+        </div>
+        <div class="columns small-12 medium-4">
+          <div class="translucent-form-overlay">
+            <form>
+              <h3>Search for Properties</h3>
+              <div class="row columns">
+                <label>Keyword
+                  <input type="text" name="keyword" placeholder="Any">
+                </label>
+              </div>
+              <div class="row columns">
+                <label>Property Status
+                  <select name="status" type="text">
+                    <option>Any</option>
+                    <option value="rent">Rent</option>
+                    <option value="buy">Buy</option>
+                  </select>
+                </label>
+              </div>
+              <div class="row columns">
+                <label>Property Type
+                  <select name="status" type="text">
+                    <option>Any</option>
+                    <option value="office">Office</option>
+                    <option value="building">Building</option>
+                  </select>
+                </label>
+              </div>
+              <div class="row columns">
+                <label>Location
+                  <input type="text" name="location" placeholder="Any">
+                </label>
+              </div>
+              <div class="row">
+                <label class="columns small-12">Price</label>
+                <div class="columns small-6">
+                  <input type="number" min="0" name="min" placeholder="Min">
+                </div>
+                <div class="columns small-6">
+                  <input type="number" min="0" name="max" placeholder="Max">
+                </div>
+              </div>
+              <button type="button" class="primary button expanded search-button">
+                Search
+              </button>
+           </form>
+          </div>
+          
+        </div>
       </div>
     </div>
-  </nav>
-</header>  
-</header>
-
-
-<main>
-
-
-<main class="form-signup">
-  <form style="position:absolute"> 
-    <img class="m-5" src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-    <h1 class="h3 mb-3 fw-normal">Login in</h1>
-
-    <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-      <label for="floatingInput">Email address</label>
-    </div>
-    <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-      <label for="floatingPassword">Password</label>
-    </div>
-
-    <div class="checkbox mb-3">
-      <label>
-        <input type="checkbox" value="remember-me"> Remember me
-      </label>
-    </div>
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-    <p class="mt-5 mb-3 text-muted">© 2017–2022</p>
-  </form>
-</main>
-</main>
-
-  
-
-  <!-- FOOTER -->
-
-  
-  <footer class="text-muted py-5">
-    <div class="container">
-        <p class="float-end mb-1">
-            <a href="#">Back to top</a>
-        </p>
-        <p class="mb-1">Album example is © Bootstrap, but please download and customize it for yourself!</p>
-        <p class="mb-0">New to Bootstrap? <a href="/">Visit the homepage</a> or read our <a href="../getting-started/introduction/">getting started guide</a>.</p>
-    </div>
-</footer>
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-
     
-</div></body><grammarly-desktop-integration data-grammarly-shadow-root="true"></grammarly-desktop-integration></html>
+    
