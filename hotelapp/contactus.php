b5802613a6b274ad42dbaa5fc775b50a19a214d6
buvanesh05 <buvaneshwaran00510@gmail.com>
2022-10-27 21:33:27 +0530

    templates

---
+++ hotelapp/contactus.php
@@ -0,0 +1,34 @@
+
+<?php
+include 'libs/load.php';
+?>
+<html lang="en">
+    <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.7.5/dist/css/foundation.min.css" crossorigin="anonymous">
+        <?php load_template('_head'); ?>
+        </head>
+        
+  <body>
+<header>
+<?php load_template('_header'); ?>
+  
+</header>
+
+
+<main>
+
+<?php load_template('_contact'); ?>
+</main>
+
+  
+
+  <!-- FOOTER -->
+
+  <?php load_template('_footer'); ?>
+
+  <script src="/hotelapp/assets/dist/js/bootstrap.bundle.min.js"></script>
+
+    </body>
+        
+</html>
--- hotelapp/templates/_head.php
+++ hotelapp/templates/_head.php
@@ -1,79 +1,136 @@
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.104.2">
-    <title>Carousel Template · Bootstrap v5.2</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+  <meta name="generator" content="Hugo 0.104.2">
+  <title>Carousel Template · Bootstrap v5.2</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/carousel/">
-
-    
-
-    
+  <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/carousel/">
 
 
 
-    <!-- Favicons -->
-<link rel="apple-touch-icon" href="/docs/5.2/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-<link rel="icon" href="/docs/5.2/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-<link rel="icon" href="/docs/5.2/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-<link rel="manifest" href="/docs/5.2/assets/img/favicons/manifest.json">
-<link rel="mask-icon" href="/docs/5.2/assets/img/favicons/safari-pinned-tab.svg" color="#712cf9">
-<link rel="icon" href="/docs/5.2/assets/img/favicons/favicon.ico">
-<meta name="theme-color" content="#712cf9">
 
 
-    <style>
-      
 
 
-.form-signup {
-    width: 100%;
-    max-width: 330px;
-    padding: 15px;
-    margin: auto;
-}
+  <!-- Favicons -->
+  <link rel="apple-touch-icon" href="/docs/5.2/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+  <link rel="icon" href="/docs/5.2/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+  <link rel="icon" href="/docs/5.2/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+  <link rel="manifest" href="/docs/5.2/assets/img/favicons/manifest.json">
+  <link rel="mask-icon" href="/docs/5.2/assets/img/favicons/safari-pinned-tab.svg" color="#712cf9">
+  <link rel="icon" href="/docs/5.2/assets/img/favicons/favicon.ico">
+  <meta name="theme-color" content="#712cf9">
 
-.form-signup .checkbox {
-    font-weight: 400;
-}
 
-.form-signup .form-floating:focus-within {
-    z-index: 2;
-}
+  <style>
+    .form-signup {
+      width: 100%;
+      max-width: 330px;
+      padding: 15px;
+      margin: auto;
+    }
 
-.form-signup input[type="email"] {
-    margin-bottom: -1px;
-    border-bottom-right-radius: 0;
-    border-bottom-left-radius: 0;
-    border-top-left-radius: 0;
-    border-top-right-radius: 0;
-}
+    .form-signup .checkbox {
+      font-weight: 400;
+    }
 
-.form-signup input[type="password"] {
-    margin-bottom: 10px;
-    border-top-left-radius: 0;
-    border-top-right-radius: 0;
-}
+    .form-signup .form-floating:focus-within {
+      z-index: 2;
+    }
 
-.form-signup input[name="phone"] {
-    margin-bottom: -1px;
-    border-bottom-right-radius: 0;
-    border-bottom-left-radius: 0;
-    border-top-left-radius: 0;
-    border-top-right-radius: 0;
-}
+    .form-signup input[type="email"] {
+      margin-bottom: -1px;
+      border-bottom-right-radius: 0;
+      border-bottom-left-radius: 0;
+      border-top-left-radius: 0;
+      border-top-right-radius: 0;
+    }
 
-.form-signup input[name="username"] {
-    margin-bottom: -1px;
-    border-bottom-right-radius: 0;
-    border-bottom-left-radius: 0;
-}
-    </style>
+    .form-signup input[type="password"] {
+      margin-bottom: 10px;
+      border-top-left-radius: 0;
+      border-top-right-radius: 0;
+    }
 
-    
-    <!-- Custom styles for this template -->
-    <link href="carousel.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-  </head>
+    .form-signup input[name="phone"] {
+      margin-bottom: -1px;
+      border-bottom-right-radius: 0;
+      border-bottom-left-radius: 0;
+      border-top-left-radius: 0;
+      border-top-right-radius: 0;
+    }
+
+    .form-signup input[name="username"] {
+      margin-bottom: -1px;
+      border-bottom-right-radius: 0;
+      border-bottom-left-radius: 0;
+    }
+
+    .translucent-form-overlay {
+      max-width: 500px;
+      width: 100%;
+      background-color: rgba(54, 54, 54, 0.8);
+      padding: 20px;
+      color: #fefefe;
+    }
+
+    .translucent-form-overlay .columns.row {
+      display: block;
+    }
+
+    .translucent-form-overlay h3,
+    .translucent-form-overlay label {
+      color: #fefefe;
+    }
+
+    .translucent-form-overlay input,
+    .translucent-form-overlay select {
+      color: #8a8a8a;
+    }
+
+    .translucent-form-overlay input::-webkit-input-placeholder {
+      color: #8a8a8a;
+    }
+
+    .translucent-form-overlay input:-ms-input-placeholder {
+      color: #8a8a8a;
+    }
+
+    .translucent-form-overlay input::placeholder {
+      color: #8a8a8a;
+    }
+
+    body {
+      background-color: #E6E8EB;
+      padding: 40px;
+    }
+
+    @media screen and (min-width: 40em) {
+      body {
+        padding: 30px;
+      }
+    }
+
+    @media screen and (min-width: 64em) {
+      body {
+        padding: 80px;
+        margin: 80px;
+      }
+
+      label {
+        text-align: left;
+        width: 100%;
+      }
+
+    }
+  </style>
+
+
+  <!-- Custom styles for this template -->
+  <link href="carousel.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+  <script src="https://cdn.jsdelivr.net/npm/foundation-sites@6.7.5/dist/js/foundation.min.js" crossorigin="anonymous"></script>
+</head>
